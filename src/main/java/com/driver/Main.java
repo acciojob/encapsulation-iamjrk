78c41655ca4bda4c9199032799ad69298a0b0551
jrk-v2 <kumar.jyotiranjan575@gmail.com>
2023-05-02 16:42:11 +0530

    Encapsulation Assignment Solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+  public static void main(String args[])
+  {
+      RWOnly obj= new RWOnly();
+      //obj.name="Jyoti Ranjan Kumar";
+      //name is private variable in class RWOnly, hence cannot access it
+//      System.out.println(obj.name);
+//      //Error: java: name has private access in com.driver.RWOnly
+      obj.setName("Jyoti Ranjan Kumar");
+      System.out.println(obj.getName());
+  }
 }
